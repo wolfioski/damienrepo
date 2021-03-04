fc090ce4cc5fe88b0d210159e8cee0c2981a3c9a
wolfioski <80064849+wolfioski@users.noreply.github.com>
2021-03-04 17:30:18 -0600

    Update damientest.bat

--- damientest.bat
+++ damientest.bat
@@ -1,8 +1,8 @@
 @echo off
 powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wolfioski/damienrepo/main/pscript.ps1 -Outfile C:\rh-setup\pscript.ps1"
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\pscript.ps1
-powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\scpower.ps1
 copy C:\rh-setup\EMRlogin.txt "C:\Program Files (x86)\EMR"
+powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\scpower.ps1
 echo Done
 pause
 
