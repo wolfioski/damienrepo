8ee995d827fce302dee886f30d1ff8035b6857de
wolfioski <80064849+wolfioski@users.noreply.github.com>
2021-03-04 14:22:08 -0600

    Add files via upload

---
+++ damientest.bat
@@ -0,0 +1,7 @@
+@echo off
+powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wolfioski/damienrepo/main/pscript.ps1 -Outfile C:\rh-setup\pscript.ps1"
+powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\psscript.ps1
+copy C:\rh-setup\EMRlogin.txt "C:\Program Files (x86)\EMR"
+echo Done
+pause
+
