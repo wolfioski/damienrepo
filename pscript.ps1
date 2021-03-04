2a18c981c5f5dcd4dea3fd2d9a8634e8873c300e
wolfioski <80064849+wolfioski@users.noreply.github.com>
2021-03-04 17:39:11 -0600

    Update pscript.ps1

--- pscript.ps1
+++ pscript.ps1
@@ -1,5 +1,6 @@
 Invoke-WebRequest https://raw.githubusercontent.com/wolfioski/damienrepo/main/EMRlogin.txt -Outfile C:\rh-setup\EMRlogin.txt
 Invoke-WebRequest https://raw.githubusercontent.com/wolfioski/damienrepo/main/Custom%20Dictionary.txt -Outfile 'C:\rh-setup\Custom Dictionary.txt'
+Invoke-WebRequest https://raw.githubusercontent.com/wolfioski/damienrepo/main/scpower.ps1 -Outfile C:\rh-setup\scpower.ps1
 $Path = $env:TEMP; $Installer = "chrome_installer.exe"; Invoke-WebRequest "http://dl.google.com/chrome/install/375.126/chrome_installer.exe" -OutFile $Path\$Installer; Start-Process -FilePath $Path\$Installer -Args "/silent /install" -Verb RunAs -Wait; Remove-Item $Path\$Installer
 $Source = 'C:\rh-setup\Custom Dictionary.txt'
 $Destination = 'C:\Users\*\AppData\Local\Google\Chrome\User Data\Default'
