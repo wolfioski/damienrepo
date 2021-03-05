0a0316504887d4df93e2f11fab1063985b55b33a
wolfioski <80064849+wolfioski@users.noreply.github.com>
2021-03-04 19:09:24 -0600

    Update damientest.bat

--- damientest.bat
+++ damientest.bat
@@ -3,7 +3,6 @@ powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wolfios
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\pscript.ps1
 copy C:\rh-setup\EMRlogin.txt "C:\Program Files (x86)\EMR"
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\scpower.ps1
-powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\copycd.ps1
+copy "C:\rh-setup\Custom Dictionary.txt" "C:\Users\%USERNAME%\AppData\Local\Google\Chrome\User Data\Default"
 echo Done
 pause
-
