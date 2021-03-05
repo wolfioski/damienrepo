9b46b406664841a86c98fceef2b957637ee8f1cc
wolfioski <80064849+wolfioski@users.noreply.github.com>
2021-03-05 00:44:20 -0600

    Update damientest.bat

--- damientest.bat
+++ damientest.bat
@@ -3,10 +3,11 @@ powershell -Command "Invoke-WebRequest https://raw.githubusercontent.com/wolfios
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\pscript.ps1
 copy C:\rh-setup\EMRlogin.txt "C:\Program Files (x86)\EMR"
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\scpower.ps1
+::Below starts chrome because file path needed to insert Custom Dictionary is not created by chrome installation it is only created from chrome starting
 start chrome
-timeout /t 4 /nobreak > nul
-taskkill /F /IM chrome.exe /T > nul
-timeout /t 10 /nobreak > nul
+timeout /t 7 /nobreak > nul
 powershell.exe -ExecutionPolicy Bypass -File c:\rh-setup\copycd.ps1
+::Below closes Chrome because Custom Dictionary while in proper folder location does not take affect until chrome is started closed and started again
+powershell -command "Get-Process chrome | ForEach-Object { $_.CloseMainWindow() | Out-Null}"
 echo Done and SUCCESFUL!!!!!
 pause
